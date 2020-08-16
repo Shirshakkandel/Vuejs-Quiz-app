b82b5be4b11530c3720deaf7e30c856948014116
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-08-16 15:51:04 +0545

    UPto question part of quiz app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <b-container class="bv-example-row">
+  <b-row>
+    <b-col sm="6" offset="3"> 
+      <QuestionBox
+      v-if="questions.length"
+      :currentQuestion="questions[index]"
+      :next="next"
+      />
+
+      </b-col>
+    
+  </b-row>
+</b-container>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import QuestionBox from './components/QuestionBox';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data(){
+    return {
+      questions:[],
+      index: 0,
+    }
+  },
+
+  methods: {
+
+    next(){
+      this.index++
+    }
+  },
+
+  mounted:function(){
+    fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple",{
+      method:'get'
+    })
+
+    .then((response)=> {
+      return response.json()
+    })
+
+    .then((jsonData) => {
+      this.questions=jsonData.results
+    })
+    
   }
+
+
 }
 </script>
 
@@ -20,9 +65,10 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -moz-osx-font-smoothing: grayscale; 
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
+  margin-top: 30px;
+
 }
 </style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="question-box">
+  <b-jumbotron>
+    <template v-slot:header></template>
+     {{currentQuestion.question}}
+    <template v-slot:lead>
+
+     
+    </template>
+
+    <hr class="my-4">
+<p>
+List of answer
+</p>
+    
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button @click="next" variant="success" >Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+   props:{
+      currentQuestion:Object,
+      next:Function
+   }
+}
+</script>
